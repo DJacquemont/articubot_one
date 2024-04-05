70a723ca462e5bafa2ce057cb3425832e43ca93f
Dimitri Jacquemont <you@example.com>
2024-04-05 16:03:22 +0200

    feat: new map living room

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -145,7 +145,7 @@ def generate_launch_description():
         ]),
         launch_arguments={
             'params_file': os.path.join(get_package_share_directory('articubot_one'), 'config', 'nav2_params.yaml'),
-            'map':os.path.join(get_package_share_directory('articubot_one'), 'maps', 'map_arena_save.yaml'),
+            'map':os.path.join(get_package_share_directory('articubot_one'), 'maps', 'map_lr_save.yaml'),
             'use_sim_time': 'false'
         }.items(),
         condition=IfCondition(LaunchConfiguration('activate_loc'))
